f16c995087f7988d57b6ee5c875746bf7ee0a941
Manuel Dafonte <jmdafonte1@gmail.com>
2022-08-09 18:15:06 +0200

    añadiendo los nuevos archivos

---
+++ javascriptHelados.js
@@ -0,0 +1,22 @@
+dineroCliente1 = prompt ("cuanto dinero tienes, cliente1?");
+dineroCliente2 = prompt ("cuanto dinero tienes, cliente2?");
+dineroCliente3 = prompt ("cuanto dinero tienes, cliente3?");
+
+if (dineroCliente1 >= 1 && dineroCliente1 < 2)
+{
+alert ("comprate un donut")
+}
+
+else if (dineroCliente2 >= 2 && dineroCliente2 < 3)
+{
+alert ("comprate una palmera de chocolate")
+}
+
+else if (dineroCliente3 >= 3)
+{
+alert ("comprate una tarta")
+}
+
+else {
+    alert("vas a morir de hambre")
+};
