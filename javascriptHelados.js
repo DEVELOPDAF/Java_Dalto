5ca5b5fbcd8e8c7d9b1b27039c558116f14324ef
Manuel Dafonte <jmdafonte1@gmail.com>
2022-08-10 08:37:28 +0200

    nuevos cambios

--- javascriptHelados.js
+++ javascriptHelados.js
@@ -1,22 +1,25 @@
-dineroCliente1 = prompt ("cuanto dinero tienes, cliente1?");
-dineroCliente2 = prompt ("cuanto dinero tienes, cliente2?");
-dineroCliente3 = prompt ("cuanto dinero tienes, cliente3?");
+dineroJuan = prompt ("cuanto dinero tienes, Juan?");
+dineroPedro = prompt ("cuanto dinero tienes, Pedro?");
+dineroSantiago = prompt ("cuanto dinero tienes, Santiago?");
 
-dineroCliente1 = parseInt(dineroCliente1);
+dineroJuan = parseInt(dineroJuan);
 
-if (dineroCliente1 >= 1 && dineroCliente1 < 2)
+if (dineroJuan >= 1 && dineroJuan < 2)
 {
 alert ("puedes comprarte uno de nata");
+alert ("puedes comprarte uno de nata y te sobran " + (dineroJuan -1.5));
 }
 
-else if (dineroCliente2 >= 2 && dineroCliente2 < 3)
+else if (dineroPedro >= 2 && dineroPedro < 3)
 {
 alert ("puedes comprarte uno de chocolate");
+alert ("puedes comprarte uno de Chocolate " + (dineroPedro -2.3));
 }
 
-else if (dineroCliente3 >= 3)
+else if (dineroSantiago >= 3)
 {
 alert ("puedes comprarte uno de limon");
+alert ("puedes comprarte uno de limon " + (dineroSantiago -3.4));
 }
 
 else {
