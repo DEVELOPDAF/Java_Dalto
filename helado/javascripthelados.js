ca206a29d36643aca3ab28c72fc4f043b6d64866
Manuel Dafonte <jmdafonte1@gmail.com>
2022-08-17 19:59:12 +0200

    algo

--- helado/javascripthelados.js
+++ helado/javascripthelados.js
@@ -4,85 +4,85 @@ dineroLuis = prompt("cuanto dinero tienes, Luis?");
 dineroJavier = prompt("cuanto dinero tienes, Javier?");
 
 if (dineroPedro >= 0.5 && dineroPedro <= 1) {
-    alert ("Pedro, comprate el helado de limón")
-    alert ("y te sobran, " + (dineroPedro - 0.2))
+    alert ("Pedro, comprate el helado de limón");
+    alert ("y te sobran, " + (dineroPedro - 0.2));
 }
 else if (dineroPedro >=2 && dineroPedro < 3) { 
-    alert (" Pedro, comprate el helado de pistacho")
-    alert ("y te sobran, " + (dineroPedro - 1.4))
+    alert (" Pedro, comprate el helado de pistacho");
+    alert ("y te sobran, " + (dineroPedro - 1.4));
 }
 else if (dineroPedro >=3 && dineroPedro < 4) { 
-    alert ("Pedro, comprate el helado de Fresa")
-    alert ("y te sobran, " + (dineroPedro - 2.6))
+    alert ("Pedro, comprate el helado de Fresa");
+    alert ("y te sobran, " + (dineroPedro - 2.6));
 }
 else if (dineroPedro >=4 ) { 
-    alert ("Pedro, comprate el helado de Coco")
-    alert ("y te sobran, " + (dineroPedro - 3.5))
+    alert ("Pedro, comprate el helado de Coco");
+    alert ("y te sobran, " + (dineroPedro - 3.5));
 }
 
 else {
-    alert ("Meh no tiene pasta, pasarás hambre")
+    alert ("Meh no tiene pasta, pasarás hambre");
 }
 
 if (dineroJuan >= 0.5 && dineroJuan <= 1) {
-    alert ("Juan, comprate el helado de limón")
-    alert ("y te sobran, " + (dineroJuan - 0.2))
+    alert ("Juan, comprate el helado de limón");
+    alert ("y te sobran, " + (dineroJuan - 0.2));
 }
 else if (dineroJuan >=2 && dineroJuan < 3) { 
-    alert (" Juan, comprate el helado de pistacho")
-    alert ("y te sobran, " + (dineroJuan - 1.4))
+    alert (" Juan, comprate el helado de pistacho");
+    alert ("y te sobran, " + (dineroJuan - 1.4));
 }
 else if (dineroJuan >=3 && dineroJuan < 4) { 
-    alert ("Juan, comprate el helado de Fresa")
-    alert ("y te sobran, " + (dineroJuan - 2.6))
+    alert ("Juan, comprate el helado de Fresa");
+    alert ("y te sobran, " + (dineroJuan - 2.6));
 }
 else if (dineroJuan >=4 ) { 
-    alert ("Juan, comprate el helado de Coco")
-    alert ("y te sobran, " + (dineroJuan - 3.5))
+    alert ("Juan, comprate el helado de Coco");
+    alert ("y te sobran, " + (dineroJuan - 3.5));
 }
 
 else {
-    alert ("Meh no tiene pasta, pasarás hambre")
+    alert ("Meh no tiene pasta, pasarás hambre");
 };
 
 if (dineroLuis >= 0.5 && dineroLuis <= 1) {
-    alert ("Luis, comprate el helado de limón")
-    alert ("y te sobran, " + (dineroLuis - 0.2))
+    alert ("Luis, comprate el helado de limón");
+    alert ("y te sobran, " + (dineroLuis - 0.2));
 }
 else if (dineroLuis >=2 && dineroLuis < 3) { 
-    alert ("Luis, comprate el helado de pistacho")
-    alert ("y te sobran, " + (dineroLuis - 1.4))
+    alert ("Luis, comprate el helado de pistacho");
+    alert ("y te sobran, " + (dineroLuis - 1.4));
 }
 else if (dineroLuis >=3 && dineroLuis < 4) { 
-    alert ("Luis, comprate el helado de Fresa")
-    alert ("y te sobran, " + (dineroLuis - 2.6))
+    alert ("Luis, comprate el helado de Fresa");
+    alert ("y te sobran, " + (dineroLuis - 2.6));
 }
 else if (dineroLuis >=4 ) { 
-    alert ("Luis, comprate el helado de Coco")
-    alert ("y te sobran, " + (dineroLuis - 3.5))
+    alert ("Luis, comprate el helado de Coco");
+    alert ("y te sobran, " + (dineroLuis - 3.5));
 }
 
 else {
-    alert ("Meh no tiene pasta, pasarás hambre")
+    alert ("Meh no tiene pasta, pasarás hambre");
 }
 
 if (dineroJavier >= 0.5 && dineroJavier <= 1) {
-    alert ("Javier, comprate el helado de limón")
-    alert ("y te sobran, " + (dineroJavier - 0.2))
+    alert ("Javier, comprate el helado de limón");
+    alert ("y te sobran, " + (dineroJavier - 0.2));
 }
 else if (dineroJavier >=2 && dineroJavier < 3) { 
-    alert (" Javier, comprate el helado de pistacho")
-    alert ("y te sobran, " + (dineroJavier - 1.4))
+    alert (" Javier, comprate el helado de pistacho");
+    alert ("y te sobran, " + (dineroJavier - 1.4));
 }
 else if (dineroJavier >=3 && dineroJavier < 4) { 
-    alert ("Javier, comprate el helado de Fresa")
-    alert ("y te sobran, " + (dineroJavier - 2.6))
+    alert ("Javier, comprate el helado de Fresa");
+    alert ("y te sobran, " + (dineroJavier - 2.6));
 }
 else if (dineroJavier >=4 ) { 
-    alert ("Javier, comprate el helado de Coco")
-    alert ("y te sobran, " + (dineroJavier - 3.5))
+    alert ("Javier, comprate el helado de Coco");
+    alert ("y te sobran, " + (dineroJavier - 3.5));
 }
 
 else {
-    alert ("Meh no tiene pasta, pasarás hambre")
+    alert ("Meh no tiene pasta, pasarás hambre");
 };
