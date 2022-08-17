ec31d7775c7f9a7c684ea57dbabaab032f931a6a
DEVELOPDAF <108474386+DEVELOPDAF@users.noreply.github.com>
2022-08-17 18:31:42 +0200

    Create javascript.js

---
+++ helado/javascript.js
@@ -0,0 +1,72 @@
+dineroPedro = prompt("cuanto dinero tienes, Pedro?");
+dineroJuan =prompt("cuanto dinero tienes, Juan?");
+dineroLuis = prompt("cuanto dinero tienes, Luis?");
+dineroJavier = prompt("cuanto dinero tienes, Javier"?);
+
+if (dineroPedro >= 0.5 && dineroPedro <= 1) {
+    alert ("Pedro, comprate el helado de limón")
+}
+else if (dineroPedro >=2 && dineroPedro < 3) { 
+    alert (" Pedro, comprate el helado de pistacho")
+}
+else if (dineroPedro >=3 && dineroPedro < 4) { 
+    alert ("Pedro, comprate el helado de Fresa")
+}
+else if (dineroPedro >=4 && dineroPedro < 5) { 
+    alert ("Pedro, comprate el helado de Coco")
+}
+
+else {
+    alert ("Meh no tiene pasta, pasarás hambre")
+}
+
+if (dineroJuan >= 0.5 && dineroJuan <= 1) {
+    alert ("Juan, comprate el helado de limón")
+}
+else if (dineroJuan >=2 && dineroJuan < 3) { 
+    alert (" Juan, comprate el helado de pistacho")
+}
+else if (dineroJuan >=3 && dineroJuan < 4) { 
+    alert ("Juan, comprate el helado de Fresa")
+}
+else if (dineroJuan >=4 && dineroJuan < 5) { 
+    alert ("Juan, comprate el helado de Coco")
+}
+
+else {
+    alert ("Meh no tiene pasta, pasarás hambre")
+};
+
+if (dineroLuis >= 0.5 && dineroLuis <= 1) {
+    alert ("Luis, comprate el helado de limón")
+}
+else if (dineroLuis >=2 && dineroLuis < 3) { 
+    alert ("Luis, comprate el helado de pistacho")
+}
+else if (dineroLuis >=3 && dineroLuis < 4) { 
+    alert ("Luis, comprate el helado de Fresa")
+}
+else if (dineroLuis >=4 && dineroLuis < 5) { 
+    alert ("Luis, comprate el helado de Coco")
+}
+
+else {
+    alert ("Meh no tiene pasta, pasarás hambre")
+}
+
+if (dineroJavier >= 0.5 && dineroJavier <= 1) {
+    alert ("Javier, comprate el helado de limón")
+}
+else if (dineroJavier >=2 && dineroJavier < 3) { 
+    alert (" Javier, comprate el helado de pistacho")
+}
+else if (dineroJavier >=3 && dineroJavier < 4) { 
+    alert ("Javier, comprate el helado de Fresa")
+}
+else if (dineroJavier >=4 && dineroJavier < 5) { 
+    alert ("Javier, comprate el helado de Coco")
+}
+
+else {
+    alert ("Meh no tiene pasta, pasarás hambre")
+};
