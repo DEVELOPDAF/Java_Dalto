49ab43778568953dd7eedf48ca626615a76bada9
Manuel Dafonte <jmdafonte1@gmail.com>
2022-08-22 19:40:24 +0200

    prueba

---
+++ Alumnos/Alumnos_Dalto.js
@@ -0,0 +1,7 @@
+virgenes = ["Pilar", "Macarena", "Rocio", "Carmen"];
+cristos = ["gitanos", "redentor", "medinaceli", "buenaMuerte"];
+
+for (let c in virgenes) {
+    if (c == "Pilar")
+    document.write(c + " " + cristos + " " );
+}
